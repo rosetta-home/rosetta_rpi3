fb812bdfb92064eeeaa821c6d612ae118010f7fa
Christopher Coté <Christopher Cot>
2018-02-15 15:24:46 -0600

    update artifact

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule NervesSystemRpi3.Mixfile do
       {:nerves, "~> 0.9", runtime: false },
       {:nerves_system_br, "0.17.0", runtime: false},
       {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 0.13.0", runtime: false},
-      {:nerves_system_linter, "~> 0.2.2", runtime: false}
+      {:nerves_system_linter, "~> 0.2.2", runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 
